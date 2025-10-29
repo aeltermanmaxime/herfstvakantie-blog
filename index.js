b0f6af03edcd65332881199eef49b283c34f26cf
aeltermanmaxime <aelterman.maxime@svsl.be>
2025-10-29 18:10:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
- alert("Welkom op mijn blog!");
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    scrollTopBtn.classList.add('visible');
+  } else {
+    scrollTopBtn.classList.remove('visible');
+  }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
